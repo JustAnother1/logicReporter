c50ac896fc759f9a97a1e16663c0522a650849d9
Lars Pötter <kontakt@ing-poetter.de>
2023-11-17 14:55:00 +0100

    Initial commit

---
+++ src/main/java/de/nomagic/ReporterMain.java
@@ -0,0 +1,122 @@
+package de.nomagic;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
+
+public class ReporterMain
+{
+
+    public ReporterMain(String[] args)
+    {
+        startLogging(args);
+    }
+
+    private void startLogging(final String[] args)
+    {
+        int numOfV = 0;
+        for(int i = 0; i < args.length; i++)
+        {
+            if(true == "-v".equals(args[i]))
+            {
+                numOfV ++;
+            }
+        }
+
+        // configure Logging
+        switch(numOfV)
+        {
+        case 0: setLogLevel("warn"); break;
+        case 1: setLogLevel("debug");break;
+        case 2:
+        default:
+            setLogLevel("trace");
+            System.out.println("Build from " + getCommitID());
+            break;
+        }
+    }
+
+    private void setLogLevel(String LogLevel)
+    {
+        final LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        try
+        {
+            final JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(context);
+            context.reset();
+            final String logCfg =
+            "<configuration>" +
+              "<appender name='STDOUT' class='ch.qos.logback.core.ConsoleAppender'>" +
+                "<encoder>" +
+                  "<pattern>%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n</pattern>" +
+                "</encoder>" +
+              "</appender>" +
+              "<root level='" + LogLevel + "'>" +
+                "<appender-ref ref='STDOUT' />" +
+              "</root>" +
+            "</configuration>";
+            ByteArrayInputStream bin;
+            try
+            {
+                bin = new ByteArrayInputStream(logCfg.getBytes("UTF-8"));
+                configurator.doConfigure(bin);
+            }
+            catch(UnsupportedEncodingException e)
+            {
+                // A system without UTF-8 ? - No chance to do anything !
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+        catch (JoranException je)
+        {
+          // StatusPrinter will handle this
+        }
+        StatusPrinter.printInCaseOfErrorsOrWarnings(context);
+    }
+
+    public static String getCommitID()
+    {
+        try
+        {
+            final InputStream s = ReporterMain.class.getResourceAsStream("/commit-id");
+            final BufferedReader in = new BufferedReader(new InputStreamReader(s));
+            final String commitId = in.readLine();
+            final String changes = in.readLine();
+            if(null != changes)
+            {
+                if(0 < changes.length())
+                {
+                    return commitId + "-(" + changes + ")";
+                }
+                else
+                {
+                    return commitId;
+                }
+            }
+            else
+            {
+                return commitId;
+            }
+        }
+        catch( Exception e )
+        {
+            return e.toString();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        ReporterMain rm = new ReporterMain(args);
+    }
+
+}

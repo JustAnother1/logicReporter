f07448011090c8d3f96bf443f3c44f689b70f47f
Lars Pötter <kontakt@ing-poetter.de>
2023-12-05 18:02:03 +0100

    made convert script easier to use.

--- ref/convert_sal.py
+++ ref/convert_sal.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
+import os
 from saleae import automation
 
-file_path = '/home/lars/prj/logic_reporter/maven/ref/saleae.sal'
-export_path = '/home/lars/prj/logic_reporter/maven/ref'
+# for this to work the first parameter must be the name of a *.sal file
+# and the Logic application must be running.
 
-# Connect to the running Logic 2 Application on port `10430`
-manager = automation.Manager(port=10430)
-with manager.load_capture(file_path) as capture:
-    capture.export_raw_data_binary(export_path)
 
-manager.close()
+def convert(file_path, export_path):
+    # Connect to the running Logic 2 Application on port `10430`
+    manager = automation.Manager(port=10430)
+    with manager.load_capture(file_path) as capture:
+        capture.export_raw_data_binary(export_path)
+    manager.close()
+
+
+
+if __name__ == '__main__':
+    if 2 > len(sys.argv):
+        print('provide file name as parameter!')
+        sys.exit(1)
+
+    cwd = os.getcwd()
+    file_path = cwd + '/' + sys.argv[1]
+    export_path = cwd + '/' + sys.argv[1][:-4]
+    print('file_path: ' + file_path)
+    print('export_path: ' + export_path)
+    os.makedirs(export_path)
+
+    convert(file_path, export_path)

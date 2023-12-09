33cc8f4993cb43682bf9e7e1650090aafa8b4514
Lars Pötter <kontakt@ing-poetter.de>
2023-12-09 23:21:03 +0100

    added signal pause detection.

--- src/main/java/de/nomagic/swd/SwdReporter.java
+++ src/main/java/de/nomagic/swd/SwdReporter.java
@@ -74,14 +74,22 @@ public class SwdReporter
             return false;
         }
 
-        double now_time = 0;
+        double now_time = 0.0;
         boolean lastStateSwclkHigh = swclk.isHighAt(0.0);
         int nextCheck = 0;
+        double lastTime = 0.0;
 
         for(int i = 0; i < swclk.getNumberEdges(); i++)
         {
             now_time = swclk.getTimeOfEdgeAfter(now_time);
             boolean dataHigh = swdio.isHighAt(now_time);
+            if(now_time - lastTime > 0.003)
+            {
+                double diff = (now_time - lastTime);
+                int pause = 0 - (int)(diff * 1000);
+                edges.add(pause);
+            }
+            lastTime = now_time;
             if(true == lastStateSwclkHigh)
             {
                 // falling edge on SWCLK
@@ -134,22 +142,30 @@ public class SwdReporter
         while(0 < edges.size())
         {
             // data is valid on rising edge (TODO?)
-            switch(edges.get(0))
+            int curEdge = edges.get(0);
+            if(0 > curEdge)
             {
-            case CLK_FALLING_DATA_1:
-            case CLK_FALLING_DATA_0:
+                out.println("Signal pause of " + Math.abs(curEdge) + " ms");
+            }
+            else
+            {
+                switch(curEdge)
+                {
+                case CLK_FALLING_DATA_1:
+                case CLK_FALLING_DATA_0:
 
-                break;
+                    break;
 
-            case CLK_RISING_DATA_1:
-                bitToPackets.add_one();
-                if(true == report_bit_level) out.append("1,");
-                break;
+                case CLK_RISING_DATA_1:
+                    bitToPackets.add_one();
+                    if(true == report_bit_level) out.append("1,");
+                    break;
 
-            case CLK_RISING_DATA_0:
-                bitToPackets.add_zero();
-                if(true == report_bit_level) out.append("0,");
-                break;
+                case CLK_RISING_DATA_0:
+                    bitToPackets.add_zero();
+                    if(true == report_bit_level) out.append("0,");
+                    break;
+                }
             }
             edges.remove(0);
         }
